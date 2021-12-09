47fbd1d7bfeb174bc22da3e6b5a60fbb03a91acd
Robin Hellström <robin1robin@hotmail.com>
2021-12-09 20:07:30 +0100

    Test 2

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,11 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
-        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        builder =>
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 });
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlite(builder.Configuration["ConnectionStrings:DefaultConnection"]));
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddDbContext<AppDBContext>(options => 
-            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -30,12 +34,12 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseAuthorization();
-
 // added to test..
 app.UseRouting();
 
 app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
 
 app.MapControllers();
 
